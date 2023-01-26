bdc338b76af74a9b03e1d6fb37d0729bb5d6899c
Caden Marinozzi <nekumelon@gmail.com>
2023-01-26 20:03:22 +0000

    Added google adsense

--- modules/index.js
+++ modules/index.js
@@ -247,7 +247,7 @@ var game = ref(database, playPath);
 var leaderboard = ref(database, 'Leaderboard');
 var dailyBoard = ref(database, dat + '-Leaderboard');
 
-let letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m', 'n', 'o', 'p', 'q','r','s','t','u','v','w','x','y','z'];
+let letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
 
 
@@ -283,8 +283,4 @@ export async function leaderBoardChecker() {
   board = board.val();
 
   console.log(board[uid]);
-  console.log('breh');
-
-  
-  
 }
